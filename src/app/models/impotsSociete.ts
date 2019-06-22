3a762e3d7b109f825b58982b8bc9cd4a5d52e820
Charfeddine12 <charfeddinebenmohamed24@gmail.com>
2019-06-22 23:26:12 +0100

    first commit

---
+++ src/app/models/impotsSociete.ts
@@ -0,0 +1,30 @@
+import { validateConfig } from '@angular/router/src/config';
+import { Loyer } from './loyer';
+import { PersoneMor } from './personeMor';
+
+export class ImpotsSociete {
+
+     ImpotsSocieteId;
+	 StockInitial;
+	 StockFinEx;
+	 ValAchatEx;
+	 CAExportation;
+	 CAglobalTTC;
+	 CAglobalHTnonCommercial;
+	 CAglobalHTConsSurPlace;
+	 MontantPrime;
+	 ResultatComptable;
+	 ResultatFiscal;
+	 BenificesDeduits;
+	 BeneficesNonImposables;
+	 ImpotsDu;
+	 MinImpots;
+	 AccomptesProvisionnels;
+	 Penalites;
+	 TaxeVisite;
+	 PrelevementSurFonds;
+	 tatus;
+     TVA;
+     Loyer : Loyer;
+     PersonneMorl : PersoneMor;
+}
